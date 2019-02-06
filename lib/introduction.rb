55c4ee774b4c072055ebcd316878782045d15c10
Thomas Edwards <thce28@gmail.com>
2019-02-06 16:04:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
 def introduction(name)
-  
+  puts "Hello, #{name}!"
 end
+
+def introduction_with_language(name, language)
+  puts "Hello, #{name}, you LIKE #{language} and are good at it!"
+end
+
+
